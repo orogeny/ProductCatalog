31b2a5b0284f7d46aebdb189ba687c9001d1311d
orogeny <paul@orogeny.net>
2018-03-25 11:42:13 +0100

    Part 1

--- library.js
+++ library.js
@@ -4,6 +4,78 @@
 
         // execute code here
 
+        var catalog = createRandomCatalog(100);
+
+        function searchAllProducts() {
+            var promise = new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve(catalog)
+                }, 1000);
+            });
+            return promise;
+        }
+
+        function searchProductsById(id) {
+            var promise = new Promise((resolve, reject) => {
+                let i = 0;
+                setTimeout(() => {
+                    while (i < catalog.length) {
+                        if (catalog[i].id == id) {
+                            resolve({ id: id, price: catalog[i].price, type: catalog[i].type });
+                        }
+                        i++;
+                    }
+                    reject("Invalid Id: " + id);
+                }, 1000);
+            });
+            return promise;
+        }
+
+        function searchProductByType(type) {
+            var promise = new Promise((resolve, reject) => {
+                let i = 0;
+                let typeArray = [];
+                let possibleTypes = [ 'Electronics', 'Book', 'Clothing', 'Food' ];
+                if (!possibleTypes.includes(type)) {
+                    reject("Invalid Type: " + type);
+                }
+                else {
+                    setTimeout(() => {
+                        while (i < catalog.length) {
+                            if (catalog[i].type == type) {
+                                typeArray.push({ id: catalog[i].id, price: catalog[i].price, type: catalog[i].type });
+                            }
+                            i++;
+                        }
+                        resolve(typeArray);
+                    }, 1000);
+                }
+            });
+            return promise;
+        }
+
+        function searchProductsByPrice(price, difference) {
+            var promise = new Promise((resolve, reject) => {
+                let i = 0;
+                let priceArray = [];
+                if (!isFinite(price)) {
+                    reject("Invalid Price: " + price);
+                }
+                else {
+                    setTimeout(() => {
+                        while (i < catalog.length) {
+                            if (Math.abs(catalog[i].price - price) < difference) {
+                                priceArray.push({ id: catalog[i].id, price: catalog[i].price, type: catalog[i].type });
+                            }
+                            i++;
+                        }
+                        resolve(priceArray);
+                    }, 1000);
+                }
+            });
+            return promise;
+        }
+
         return {
             searchProductById: searchProductById,
             searchProductByPrice: searchProductByPrice,
@@ -18,6 +90,15 @@
             var type = typeArray[Math.floor(Math.random() * 4)];
 
             return {price: price, type: type};
+        }
+
+        function createRandomCatalog(num) {
+            var catalog = [];
+            for (var i = 0; i < num; i++) {
+                var obj = createRandomProduct();
+                catalog.push({ id: i, price: obj.price, type: obj.type });
+            }
+            return catalog;
         }
 
     }
